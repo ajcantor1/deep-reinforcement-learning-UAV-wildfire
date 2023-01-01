df1b9a97a0a4aff6f95eea35394ef5212a175641
jeremy <ajcantor1@gmail.com>
2023-01-01 12:35:55 -0500

    Add mappo

--- environments/shared_wildfire_gym.py
+++ environments/shared_wildfire_gym.py
@@ -17,15 +17,15 @@ class SharedWildFireGym(Env):
 
         self.action_space = [spaces.Discrete(2) for _ in range(self.n_agents)]
 
-        self.observation_space = [spaces.Dict(
-                belief_map = spaces.Box(low=0, high=1.0, shape=(2, HEIGHT, WIDTH), dtype=np.float32),
-                bank_angle = spaces.Box(low=-0.872665, high=0.872665, shape=(1,), dtype=np.float32),
-                rho = spaces.Box(low=0, high=141.421, shape=(1,), dtype=np.float32),
-                theta = spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float32),
-                psi = spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float32),
-                other_bank_angle = spaces.Box(low=-0.872665, high=0.872665, shape=(1,), dtype=np.float32)
-            )
-            for _ in range(self.n_agents) ]
+        self.observation_space = spaces.Dict(
+            belief_map = spaces.Box(low=0, high=1.0, shape=(2, HEIGHT, WIDTH), dtype=np.float32),
+            bank_angle = spaces.Box(low=-0.872665, high=0.872665, shape=(1,), dtype=np.float32),
+            rho = spaces.Box(low=0, high=141.421, shape=(1,), dtype=np.float32),
+            theta = spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float32),
+            psi = spaces.Box(low=-np.pi, high=np.pi, shape=(1,), dtype=np.float32),
+            other_bank_angle = spaces.Box(low=-0.872665, high=0.872665, shape=(1,), dtype=np.float32)
+        )
+   
         self.fireEnv = ProbabilisticFireEnv(HEIGHT, WIDTH)
         self.dronesEnv = DronesEnv(HEIGHT, WIDTH, DT, DTI) 
         self.info = {}
