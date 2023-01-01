72236f50baecf2b5d96a581dce6ce32de3016419
jeremy <ajcantor1@gmail.com>
2023-01-01 12:26:00 -0500

    Add mappo

--- environments/util/observation_space.py
+++ environments/util/observation_space.py
@@ -3,8 +3,8 @@ import gym
 
 class MultiAgentObservationSpace(list):
     def __init__(self, agents_observation_space):
-        for agent_observation in agents_observation_space:
-            assert isinstance(agent_observation, gym.spaces.space.Space)
+        for x in agents_observation_space:
+            assert isinstance(x, gym.spaces.space.Space)
 
         super().__init__(agents_observation_space)
         self._agents_observation_space = agents_observation_space
@@ -12,3 +12,11 @@ class MultiAgentObservationSpace(list):
     def sample(self):
         """ samples observations for each agent from uniform distribution"""
         return [agent_observation_space.sample() for agent_observation_space in self._agents_observation_space]
+
+    def contains(self, obs):
+        """ contains observation """
+        for space, ob in zip(self._agents_observation_space, obs):
+            if not space.contains(ob):
+                return False
+        else:
+            return True
